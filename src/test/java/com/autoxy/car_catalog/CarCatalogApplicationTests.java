e968a8b87159f3daeebf8444e84cad5c2276e69c
ggalanteCode <ggalante3@gmail.com>
2025-02-01 17:01:23 +0100

    Initial commit

---
+++ src/test/java/com/autoxy/car_catalog/CarCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoxy.car_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
