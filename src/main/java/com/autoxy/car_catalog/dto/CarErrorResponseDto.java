de472282b4995af9625c493ae19e86ee4fb1f80c
ggalanteCode <ggalante3@gmail.com>
2025-08-12 00:36:52 +0200

    refactor: convert car DTOs to records.

--- src/main/java/com/autoxy/car_catalog/dto/CarErrorResponseDto.java
+++ src/main/java/com/autoxy/car_catalog/dto/CarErrorResponseDto.java
@@ -1,19 +1,3 @@
 package com.autoxy.car_catalog.dto;
 
-public class CarErrorResponseDto {
-
-    private String message;
-
-    public CarErrorResponseDto(String message) {
-        super();
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+public record CarErrorResponseDto(String message) {}
